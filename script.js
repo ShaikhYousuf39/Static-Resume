57a6c9657bbd91f258f79eaec72a896176aca560
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-09-23 19:19:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Toggle the visibility of the skills section
+var toggleButton = document.getElementById('toggle-skills');
+var skillsSection = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (skillsSection.style.display === 'none') {
+        skillsSection.style.display = 'block';
+    }
+    else {
+        skillsSection.style.display = 'none';
+    }
+});
